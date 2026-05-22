8f5eb106f72452a8750935da336c4462f58fb37e
shogo.takasaki <stakasaki707@gmail.com>
2026-05-22 23:10:45 +0900

    Initial commit

---
+++ app/src/main/java/jp/shirataki707/playground/NavigationKeys.kt
@@ -0,0 +1,6 @@
+package jp.shirataki707.playground
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable data object Main : NavKey
---
+++ app/src/main/java/jp/shirataki707/playground/data/DataRepository.kt
@@ -0,0 +1,12 @@
+package jp.shirataki707.playground.data
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+interface DataRepository {
+  val data: Flow<List<String>>
+}
+
+class DefaultDataRepository : DataRepository {
+  override val data: Flow<List<String>> = flow { emit(listOf("Android")) }
+}
